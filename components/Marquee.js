2f27efd0239965a296b3b24239921def1923a213
pratapbhim <bhimpratap08@gmail.com>
2025-12-13 13:59:46 +0530

    final touch no err

--- components/Marquee.js
+++ components/Marquee.js
@@ -5,7 +5,7 @@ export default function Marquee() {
   return (
     <div className="w-full bg-gradient-to-r from-red-600 to-red-800 dark:from-red-900 dark:to-yellow-900 dark:bg-red-900 text-white py-2 shadow-sm overflow-hidden whitespace-nowrap relative border-b-0">
       <div className="inline-block font-semibold text-base px-0 animate-marquee">
-        🚀 India’s Most Affordable Delivery Partner • Save Up to 50% on Commission • 24/7 Dedicated Support • Smart & Flexible Delivery Solutions • Grow your business faster with our zero-commission platform, 21 days of free service, and a complimentary advertising package that gives your brand the spotlight it deserves • 
+        🚀✨ India’s Most Affordable Delivery Partner • Save Up to 50% on Commission • 24/7 Dedicated Support • Smart & Flexible Delivery Solutions • Grow your business faster with our zero-commission platform, 21 days of free service, and a complimentary advertising package that gives your brand the spotlight it deserves • 
       </div>
       <style>{`
         @keyframes marquee {
